383103e592d09489b3a59f90e724088f7d210201
Fengda HUANG <h@phodal.com>
2016-04-22 08:23:27 +0800

    add word cloud

---
+++ generate_text_cloud.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+"""
+Minimal Example
+===============
+Generating a square wordcloud from the US constitution using default arguments.
+"""
+
+from os import path
+from wordcloud import WordCloud
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(path.join(d, 'article.txt')).read()
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud)
+plt.axis("off")
+
+# take relative word frequencies into account, lower max_font_size
+wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text)
+plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
+# The pil way (if you don't have matplotlib)
+#image = wordcloud.to_image()
+#image.show()
