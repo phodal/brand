4944db7991f57a555f327531b9e73d40b63e459b
Fengda HUANG <h@phodal.com>
2016-04-22 17:21:21 +0800

    merge for idea

--- generate.py
+++ generate.py
@@ -3,13 +3,32 @@ import svgwrite
 
 def generate_idea():
     dwg = svgwrite.Drawing('shields/idea.svg', profile='full', size=(u'1006', u'150'))
+    y_text_split = 641
+    height = 150
+    rect_length = 10
+    width = 1206
+    max_rect_length = 40
 
     shapes = dwg.add(dwg.g(id='shapes', fill='none'))
     shapes.add(dwg.rect((0, 0), (640, 150), fill='#5E6772'))
     shapes.add(dwg.rect((640, 0), (366, 150), fill='#2196F3'))
     shapes.add(dwg.text('PHODAL', insert=(83, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
-    shapes.add(dwg.text('idea', insert=(704, 122), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
 
+    for x in range(y_text_split + rect_length, width, rect_length):
+        shapes.add(dwg.line((x, 0), (x, height), stroke='#EEEEEE', stroke_opacity=0.3))
+
+    for y in range(rect_length, height, rect_length):
+        shapes.add(dwg.line((y_text_split, y), (width, y), stroke='#EEEEEE', stroke_opacity=0.3))
+
+    for x in range(y_text_split + max_rect_length, width, max_rect_length):
+        for y in range(0, height, max_rect_length):
+            shapes.add(dwg.line((x, y - 4), (x, y + 4), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
+
+    for y in range(0, height, max_rect_length):
+        for x in range(y_text_split + max_rect_length, width, max_rect_length):
+            shapes.add(dwg.line((x - 4, y), (x + 4, y), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
+
+    shapes.add(dwg.text('idea', insert=(704, 122), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
     dwg.save()
 
 
