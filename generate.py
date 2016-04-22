937f9352a4c314a72a86c1f76ba35e0f7631e3c5
Fengda HUANG <h@phodal.com>
2016-04-22 15:52:29 +0800

    change images

--- generate.py
+++ generate.py
@@ -35,6 +35,19 @@ def generate_works():
 
   dwg.save()
 
+
+def generate_works():
+  dwg = svgwrite.Drawing('shields/design.svg', size=(u'1126', u'150'))
+
+  shapes = dwg.add(dwg.g(id='shapes', fill='none'))
+  shapes.add(dwg.rect((0, 0), (640, 150), fill='#5E6772'))
+  shapes.add(dwg.rect((640, 0), (486, 150), fill='#2196F3'))
+  shapes.add(dwg.text('PHODAL', insert=(83, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
+  shapes.add(dwg.text('design', insert=(704, 122), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
+
+  dwg.save()
+
 generate_idea()
 generate_article()
+generate_works()
 generate_works()
