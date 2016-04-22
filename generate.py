01819e40c9affc0865cfdf13bd7f3d846a0ec224
Fengda HUANG <h@phodal.com>
2016-04-22 16:59:49 +0800

    add blueprint

--- generate.py
+++ generate.py
@@ -50,18 +50,32 @@ def generate_design():
 
 
 def generate_cool_deisgn():
+    y_text_split = 841
+    height = 150
+    rect_length = 10
+    width = 1206
+    max_rect_length = 40
+
     dwg = svgwrite.Drawing('shields/cool-idea.svg', profile='full', size=(u'1206', u'150'))
-
     shapes = dwg.add(dwg.g(id='shapes', fill='none'))
-    shapes.add(dwg.rect((0, 0), (841, 150), fill='#5E6772'))
-    shapes.add(dwg.rect((841, 0), (366, 150), fill='#2196F3'))
-    shapes.add(dwg.text('PHODAL', insert=(-18, 150), fill='#FFFFFF', font_size=210, font_family='Helvetica'))
+    shapes.add(dwg.rect((0, 0), (y_text_split, height), fill='#5E6772'))
+    shapes.add(dwg.rect((y_text_split, 0), (366, height), fill='#2196F3'))
+    shapes.add(dwg.text('PHODAL', insert=(-18, height), fill='#FFFFFF', font_size=210, font_family='Helvetica'))
 
-    for x in range(861, 1206, 20):
-        shapes.add(dwg.line((x, 0), (x, 150), stroke='#EEEEEE'))
+    for x in range(y_text_split + rect_length, width, rect_length):
+        shapes.add(dwg.line((x, 0), (x, height), stroke='#EEEEEE'))
 
-    for y in range(20, 150, 20):
-        shapes.add(dwg.line((841, y), (1206, y), stroke='#EEEEEE'))
+    for y in range(rect_length, height, rect_length):
+        shapes.add(dwg.line((y_text_split, y), (width, y), stroke='#EEEEEE'))
+
+    for x in range(y_text_split + max_rect_length, width, max_rect_length):
+        for y in range(0, height, max_rect_length):
+            shapes.add(dwg.line((x, y - 3), (x, y + 3), stroke='#EEEEEE', stroke_width='3'))
+
+    for y in range(0, height, max_rect_length):
+        for x in range(y_text_split + max_rect_length, width, max_rect_length):
+            print x, y, max_rect_length
+            shapes.add(dwg.line((x - 3, y), (x + 3, y), stroke='#EEEEEE', stroke_width='3'))
 
     shapes.add(dwg.text('idea', insert=(920, 122), fill='#fff', font_size=120, font_family='Helvetica'))
     dwg.save()
