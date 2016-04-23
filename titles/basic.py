fee8f7216757b26c49501f5c6fd6746dce32220b
Fengda HUANG <h@phodal.com>
2016-04-23 15:10:34 +0800

    tried to try it............:cry::cry::cry::cry::cry::cry::cry::cry::cry::cry::cry::cry:

--- titles/basic.py
+++ titles/basic.py
@@ -6,12 +6,12 @@ def generate_works():
 
     shapes = dwg.add(dwg.g(id='shapes', fill='none'))
 
-    shapes.add(dwg.rect((150, 150), (220, 150), fill='#59B840', transform="rotate(-15 0, 0)"))
-    shapes.add(dwg.text('01', insert=(220, 230), fill='#fff', font_size=120, font_family='Helvetica', transform="rotate(-5 0, 0)"))
-    shapes.add(dwg.rect((180, 0), (50, 300), fill='#fff'))
-    shapes.add(dwg.rect((380, 0), (80, 300), fill='#fff'))
+    shapes.add(dwg.line((150, 250), (380, 250), stroke='#1A7906', stroke_width=140, transform="rotate(-15 0, 0)"))
 
-    shapes.add(dwg.rect((420, 80), (600, 250), fill='#59B840'))
+    shapes.add(dwg.rect((240, 20), (200, 150), fill='#59B840', transform="rotate(15 0, 0)"))
+    shapes.add(dwg.text('01', insert=(220, 230), fill='#fff', font_size=120, font_family='Helvetica', transform="rotate(-5 0, 0)"))
+
+    shapes.add(dwg.rect((413, 76), (600, 250), fill='#59B840'))
 
     shapes.add(dwg.text('标题', insert=(450, 160), fill='#fff', font_size=70,
                         font_family='Helvetica'))
@@ -20,5 +20,5 @@ def generate_works():
     dwg.save()
 
 
-
-generate_works()
+if __name__ == '__main__':
+    generate_works()
