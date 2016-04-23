3e7c964a25b62f48fb7d9f917ab7f23efceb18e8
Fengda HUANG <h@phodal.com>
2016-04-23 09:24:46 +0800

    done for ideas

--- generate.py
+++ generate.py
@@ -2,37 +2,41 @@ import random
 
 import svgwrite
 
-#phodal_width = 640
 phodal_width = 528
+secondary_text_x = 588
 
 def generate_idea():
-    dwg = svgwrite.Drawing('shields/idea.svg', profile='full', size=(u'1006', u'150'))
     y_text_split = phodal_width + 1
     height = 150
     rect_length = 10
-    width = 1206
+    width = 868
     max_rect_length = 40
 
+    dwg = svgwrite.Drawing('shields/idea.svg', profile='full', size=(u'868', u'150'))
+
     shapes = dwg.add(dwg.g(id='shapes', fill='none'))
-    shapes.add(dwg.rect((0, 0), (phodal_width, 150), fill='#5E6772'))
-    shapes.add(dwg.rect((phodal_width, 0), (366, 150), fill='#2196F3'))
+    shapes.add(dwg.rect((0, 0), (phodal_width, height), fill='#5E6772'))
+    shapes.add(dwg.rect((phodal_width, 0), (width - phodal_width, height), fill='#2196F3'))
     shapes.add(dwg.text('phodal', insert=(83, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
 
-    for x in range(y_text_split + rect_length, width, rect_length):
-        shapes.add(dwg.line((x, 0), (x, height), stroke='#EEEEEE', stroke_opacity=0.3))
+    def draw_for_bg_plus():
+        for x in range(y_text_split + rect_length, width, rect_length):
+            shapes.add(dwg.line((x, 0), (x, height), stroke='#EEEEEE', stroke_opacity=0.3))
 
-    for y in range(rect_length, height, rect_length):
-        shapes.add(dwg.line((y_text_split, y), (width, y), stroke='#EEEEEE', stroke_opacity=0.3))
+        for y in range(rect_length, height, rect_length):
+            shapes.add(dwg.line((y_text_split, y), (width, y), stroke='#EEEEEE', stroke_opacity=0.3))
 
-    for x in range(y_text_split + max_rect_length, width, max_rect_length):
-        for y in range(0, height, max_rect_length):
-            shapes.add(dwg.line((x, y - 4), (x, y + 4), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
-
-    for y in range(0, height, max_rect_length):
         for x in range(y_text_split + max_rect_length, width, max_rect_length):
-            shapes.add(dwg.line((x - 4, y), (x + 4, y), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
+            for y in range(0, height, max_rect_length):
+                shapes.add(dwg.line((x, y - 4), (x, y + 4), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
 
-    shapes.add(dwg.text('idea', insert=(704, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
+        for y in range(0, height, max_rect_length):
+            for x in range(y_text_split + max_rect_length, width, max_rect_length):
+                shapes.add(dwg.line((x - 4, y), (x + 4, y), stroke='#EEEEEE', stroke_width='2', stroke_opacity=0.4))
+
+    draw_for_bg_plus()
+
+    shapes.add(dwg.text('idea', insert=(secondary_text_x, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
     dwg.save()
 
 
@@ -91,7 +95,6 @@ def generate_design():
     dwg = svgwrite.Drawing('shields/design.svg', size=(u'1000', u'150'))
 
     # for D Rect
-    d_line_x = 588
     red_point = 798
 
     shapes = dwg.add(dwg.g(id='shapes', fill='none'))
@@ -99,21 +102,21 @@ def generate_design():
 
     def draw_design_word():
         shapes.add(dwg.rect((phodal_width, 90), (486, 60), fill='#2196f3'))
-        shapes.add(dwg.text('design', insert=(d_line_x + 6, 120), fill='#000', stroke_width=4, font_size=120, font_family='Helvetica'))
+        shapes.add(dwg.text('design', insert=(secondary_text_x + 6, 120), fill='#000', stroke_width=4, font_size=120, font_family='Helvetica'))
         shapes.add(dwg.rect((phodal_width, 0), (486, 90), fill='#03a9f4'))
         #shapes.add(dwg.rect((phodal_width, 88), (486, 3), fill='#e91e63'))
         shapes.add(dwg.rect((phodal_width, 90), (486, 0.2), fill='#000'))
-        shapes.add(dwg.text('design', insert=(d_line_x + 4, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
+        shapes.add(dwg.text('design', insert=(secondary_text_x + 4, 119), fill='#FFFFFF', font_size=120, font_family='Helvetica'))
 
     def draw_red_point():
         shapes.add(dwg.ellipse((red_point, 40), (9, 9), fill='#000'))
         shapes.add(dwg.ellipse((red_point + 1, 39), (9, 9), fill='#f44336'))
 
     def draw_d_arround():
-        shapes.add(dwg.line((d_line_x, 90), (d_line_x, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
-        shapes.add(dwg.line((d_line_x + 70, 90), (d_line_x + 70, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
+        shapes.add(dwg.line((secondary_text_x, 90), (secondary_text_x, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
+        shapes.add(dwg.line((secondary_text_x + 70, 90), (secondary_text_x + 70, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
         #shapes.add(dwg.line((700, 25), (770, 25), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
-        shapes.add(dwg.line((d_line_x, 130), (d_line_x + 70, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
+        shapes.add(dwg.line((secondary_text_x, 130), (secondary_text_x + 70, 130), stroke='#EEEEEE', stroke_width='1', stroke_opacity=1, stroke_dasharray="2,2"))
 
     draw_design_word()
     draw_red_point()
