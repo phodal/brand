4ee5ef9967936cde94e8f57f65847a0eb9445da6
Fengda HUANG <h@phodal.com>
2016-04-23 23:26:37 +0800

    get font colors

--- cover/generate_cover_masked.py
+++ cover/generate_cover_masked.py
@@ -4,6 +4,10 @@ from os import path
 import numpy as np
 from PIL import Image
 from wordcloud import WordCloud
+import ConfigParser
+
+ConfigColor = ConfigParser.ConfigParser()
+ConfigColor.read("./color.ini")
 
 d = path.dirname(__file__)
 
@@ -29,6 +33,24 @@ wc = WordCloud(background_color=None, max_words=2000, mask=alice_mask, max_font_
                width=1423, height=601, mode="RGBA")
 # generate word cloud
 wc.generate(text)
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+def get_font_color(bg):
+    color = bg[0], bg[1], bg[2]
+    bg_hex = rgb_to_hex(color)
+    for color_name, color in ConfigColor.items('Color'):
+        color_bg = color.split(',')[0]
+        if color_bg == bg_hex:
+            return color.split(',')[1]
+
+font_color = hex_to_rgb(get_font_color(bg_color))
 
 text_cloud_image = wc.to_image()
 
